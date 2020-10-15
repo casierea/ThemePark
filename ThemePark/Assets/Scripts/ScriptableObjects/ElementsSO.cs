d61979b22583dbd2e9028b34b923599bb146db7a
DESKTOP <stephenmmichie@gmail.com>
2020-10-14 19:50:35 -0600

    Element, POI, Rewards Scriptable Objects
    made them

---
+++ ThemePark/Assets/Scripts/ScriptableObjects/ElementsSO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class ElementsSO : ScriptableObject
+{
+    public String Name, WebsiteAddress, Description, Menu;
+    public Image Icon, Logo, Pictures;
+    public Color _borderColor;
+}
