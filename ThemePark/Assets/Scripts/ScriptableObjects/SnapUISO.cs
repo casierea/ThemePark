0bebfa74d2e8fb570bc2589b7c6d9273250633c9
ben <benjaminshurts@gmail.com>
2020-10-28 21:37:11 -0600

    tool tests

--- ThemePark/Assets/Scripts/ScriptableObjects/SnapUISO.cs
+++ ThemePark/Assets/Scripts/ScriptableObjects/SnapUISO.cs
@@ -1,8 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu]
 public class SnapUISO : ScriptableObject
 {
+    public List<Vector3> _positions = new List<Vector3>();
+    //[SerializeField]
+    //private Dictionary<string, Vector3> snapPositions = new Dictionary<string, Vector3>();
+    //[Serializable]
+   
     
 }
