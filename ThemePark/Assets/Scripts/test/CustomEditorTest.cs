7e347638e2aa249fb06c28b9d5416d0d94b8dc8b
ben <benjaminshurts@gmail.com>
2020-12-05 12:37:54 -0700

    more

--- ThemePark/Assets/Scripts/test/CustomEditorTest.cs
+++ ThemePark/Assets/Scripts/test/CustomEditorTest.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-public class SnapCameraBehaviour : MonoBehaviour
+public class CustomEditorTest : MonoBehaviour
 {
     #region -------------- Variables --------------
 
