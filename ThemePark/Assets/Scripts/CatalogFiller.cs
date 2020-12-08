4daf2dd457afca0b161d1d5ef556384cefc1ba6a
ben <benjaminshurts@gmail.com>
2020-12-08 13:43:30 -0700

    Snap camera, selection system
    Reorganized a few things and set up selection system using unity events.

--- ThemePark/Assets/Scripts/CatalogFiller.cs
+++ ThemePark/Assets/Scripts/CatalogFiller.cs
@@ -1,4 +1,4 @@
-﻿using UnityEditor.Experimental.GraphView;
+﻿//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
---
+++ ThemePark/Assets/Scripts/Prefab Stuff/POIPrefabFiller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class POIPrefabFiller : MonoBehaviour
+{
+    #region -------------- Variables --------------
+
+    //public PrefebElement[] prefabStruct;
+    
+    [System.Serializable]           // Expose the struct inside the Unity Inspector
+    public struct PrefabElement
+    {
+        public GameObject prefab;
+        public string tag;
+    }
+
+    #endregion
+
+    #region -------------- Setup --------------
+
+
+
+    #endregion
+
+    #region -------------- Work --------------
+
+
+
+    #endregion
+
+    #region -------------- Methods --------------
+
+
+
+    #endregion
+
+    #region -------------- Visualize --------------
+
+
+
+    #endregion
+
+    #region -------------- Inspector --------------
+
+
+
+    #endregion
+
+}
